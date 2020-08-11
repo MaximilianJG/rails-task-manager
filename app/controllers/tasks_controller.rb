acd3a72d5a6b7d67a3d79bd56272db66c6d06050
Maximilian Glasmacher <maximilian@glasmacher.com>
2020-08-11 14:57:59 +0200

    Finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
 
+  # An option to get rid of the "@task = Task.find(params[:id])" in many of the actions below
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -8,4 +11,38 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new # don't quite understand why this needs to be there.
+  end
+
+  def create
+    @task = Task.create(strong_task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(strong_task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def strong_task_params # You specify which keys in the params hash you allow
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  # def before_action
+  #   @task = Task.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 # Read all tasks
-get '/tasks', to: 'tasks#index'
-
-
+get '/tasks', to: 'tasks#index' # displays all tasks
 # Create a new task
-get '/task/new', to: 'tasks#new' # displays an empty form
+get '/tasks/new', to: 'tasks#new' # displays an empty form
 post '/tasks', to: 'tasks#create' # creates a new instance
-
-
 #Read one task
 get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-
 # Update a task
-get '/tasks/:id/edit', to: 'tasks#edit' # displays a prefilled form
+get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # displays a prefilled form
 patch '/tasks/:id', to: 'tasks#update' # patch a new undated instance to server
 # Delete a task
-delete '/tasks/:id', to: 'tasks#destroy'
-
-
+delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy_task' # deletes the task
 end
